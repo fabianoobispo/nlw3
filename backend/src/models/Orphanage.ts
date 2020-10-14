88a5a9aa46349dd4d9da6d7191cb806d3f5c68d9
fabianoobispo <fbc623@gmail.com>
2020-10-14 20:44:19 -0300

    criado models e views, e tratamento de erros

--- backend/src/models/Orphanage.ts
+++ backend/src/models/Orphanage.ts
@@ -1,7 +1,8 @@
-import { Entity, Column, PrimaryGeneratedColumn} from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany, JoinColumn} from 'typeorm';
+import Image from './Image'
 
 @Entity('orphanages')
-export default class Ophanage {
+export default class Orphanage {
     @PrimaryGeneratedColumn('increment')
     id: number;
 
@@ -25,5 +26,11 @@ export default class Ophanage {
 
     @Column()
     open_on_weekends: boolean;
+
+    @OneToMany(() => Image, image => image.orphanage, {
+        cascade: ['insert', 'update']
+    })
+    @JoinColumn({ name: 'orphanage_id'})
+    images: Image[];
 }
 
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,13 @@
 import {Router} from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload'
 import OrphanagesControlller from './controllers/Orphanagescontreollers';
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
-routes.post('/orphanages', OrphanagesControlller.create);
+routes.post('/orphanages', upload.array('images') ,OrphanagesControlller.create);
 routes.get('/orphanages', OrphanagesControlller.index);
 routes.get('/orphanages/:id', OrphanagesControlller.show);
 
